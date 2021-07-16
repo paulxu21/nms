38caf4f5b082f6b1255d529cd868adbd63a2a057
xuhaipeng <paul_xu21@hotmail.com>
2021-07-16 09:11:48 +0800

    20210716 001 init nms

---
+++ src/test/java/com/chuyue/nms/NmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuyue.nms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
