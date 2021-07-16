434fe396554b61d532f27b1d6192d0760a3bb49e
xuhaipeng <paul_xu21@hotmail.com>
2021-07-16 17:58:31 +0800

    20210716 007 add CommonResponse

---
+++ src/main/java/com/chuyue/nms/controller/EbookController.java
@@ -0,0 +1,29 @@
+package com.chuyue.nms.controller;
+
+import com.chuyue.nms.entity.Ebook;
+import com.chuyue.nms.response.CommonResponse;
+import com.chuyue.nms.service.impl.EbookServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ebook")
+public class EbookController {
+
+    @Autowired
+    private EbookServiceImpl ebookService;
+
+    @GetMapping("/list")
+    public CommonResponse list() {
+        CommonResponse<List<Ebook>> response = new CommonResponse<>();
+        List<Ebook> list = ebookService.list();
+        response.setSuccess(true);
+        response.setMessage("返回eBooks");
+        response.setContent(list);
+        return response;
+    }
+}
