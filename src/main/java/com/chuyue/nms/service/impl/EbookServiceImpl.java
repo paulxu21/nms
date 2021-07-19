f558b4c8817bcd2b82d9c80a98112d0bcccfe784
xuhaipeng <paul_xu21@hotmail.com>
2021-07-19 09:00:11 +0800

    20210719 010 use CopyUtil modified EbookServiceImpl

--- src/main/java/com/chuyue/nms/service/impl/EbookServiceImpl.java
+++ src/main/java/com/chuyue/nms/service/impl/EbookServiceImpl.java
@@ -7,6 +7,7 @@ import com.chuyue.nms.mapper.EbookMapper;
 import com.chuyue.nms.request.EbookReq;
 import com.chuyue.nms.response.EbookResp;
 import com.chuyue.nms.service.EbookService;
+import com.chuyue.nms.utils.CopyUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,12 +26,18 @@ public class EbookServiceImpl extends ServiceImpl<EbookMapper, Ebook> implements
             wrapper.like("name", req.getName());
         }
         List<Ebook> list = ebookMapper.selectList(wrapper);
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : list) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
-        }
+//        List<EbookResp> respList = new ArrayList<>();
+//        for (Ebook ebook : list) {
+////            EbookResp ebookResp = new EbookResp();
+////            BeanUtils.copyProperties(ebook, ebookResp);
+              // 对象复制
+//            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+//            respList.add(ebookResp);
+//        }
+
+        // 列表复制
+        List<EbookResp> respList = CopyUtil.copyList(list, EbookResp.class);
+
         return respList;
     }
 }
